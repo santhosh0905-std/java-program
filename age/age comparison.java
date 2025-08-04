f75fd16cbe25a2d6d46b58e53f312b155370c3f1
santhosh0905-std <usanthosh0905@gmail.com>
2025-08-04 12:37:43 +0530

    Add files via upload

---
+++ age/age comparison.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        int A=19;
+        int S=20;
+        if (S>A){
+            System.out.println("older");
+        }
+        else{
+            System.out.println("younger");
+        }
+    }
+    
+}
