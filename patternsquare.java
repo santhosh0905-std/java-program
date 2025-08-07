c0d9b0f960000ca446355a6b2eb912359ff51743
santhosh0905-std <usanthosh0905@gmail.com>
2025-08-07 10:39:09 +0530

    Create pattern square.java

---
+++ patternsquare.java
@@ -0,0 +1,14 @@
+class Main{
+    public static void main (String[]args){
+        int n=10;
+        int a=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print((a*a)+" ");
+                a++;
+            }
+            System.out.println("");
+        }
+    }
+}
