9dffacce3ef54c3f3d590c46480ddbe4f47e2fe8
Santhosh.U <usanthosh0905@gmail.com>
2025-08-12 15:20:45 +0530

    Update String Methods.java

--- String Methods.java
+++ String Methods.java
@@ -46,3 +46,60 @@ class Main{
         
     }
 }
+
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner data=new Scanner(System.in);
+        System.out.println("original string(os):");
+        String os=data.nextLine();
+        System.out.println("copy of os:");
+        String cs=data.nextLine();
+        System.out.println(os.contains(cs));
+        System.out.println(os.contentEquals(cs));
+    }
+}
+
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner data=new Scanner(System.in);
+        System.out.println("original string(os):");
+        String os=data.nextLine();
+        System.out.println("copy of os(cs):");
+        String cs=data.nextLine();
+        System.out.println("equalsIgnoreCase():"+os.equalsIgnoreCase(cs));
+        String f ="This is %s I'm %d years old.";
+        System.out.println("sname:");
+        String sn=data.nextLine();
+        System.out.println("sage:");
+        int sa=data.nextInt();
+        String result = String.format(f,sn,sa);
+        System.out.println("format():"+result);
+    }
+}
+
+import java.util.Scanner;
+class Main{
+    public static void main(String[]args){
+        Scanner data=new Scanner(System.in);
+        String n="Santhosh";
+        byte[]b=n.getBytes();
+        System.out.println("Avalue:”+b[1]);
+        System.out.println("Hashcode:"+n.hashCode());
+    }
+}
+
+class Main{
+    public static void main(String[]args){
+        String s="santhosh";
+        System.out.println(s.indexOf("h"));
+    }
+}
+
+class Main{
+    public static void main(String[]args){
+        String n=String.join("Anbu"," ","Sotta");
+        System.out.println(n);
+    }
+}
